0a797a3ff83385d85a9d61c85859f1576587b269
Bayandin-Kirill <bayandin.kirill123@gmail.com>
2023-12-01 11:35:23 +0500

    тесты 5 лабы

---
+++ Lab_OOP_9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_OOP_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Lab_OOP_9/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_OOP_9
+{
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+        private static int count = 0;
+        //конструктор
+        public Triangle(double a = 3, double b = 4, double c = 5)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            count++;
+        }
+        // число созданных объектов
+        public static int Count() => count;
+        public static void CounterErrase() => count = 0;
+        public void Print()
+        {
+            Console.WriteLine($"a: {a}\n  b: {b}\n  c: {c}");
+            Console.WriteLine($"Кол-во созданных в программе объектов: {count}");
+        }
+        //возвращает полупериметр
+        private static double CalculationP(Triangle side)
+        {
+            return (side.a + side.b + side.c) / 2;
+        }
+        //вычисляет площадь
+        public double CalculationS()
+        {
+            return Math.Sqrt(CalculationP(this) * (CalculationP(this) - a) * (CalculationP(this) - b) * (CalculationP(this) - c));
+        }
+        public static double CalculationS(Triangle side)
+        {
+            return Math.Sqrt(CalculationP(side) * (CalculationP(side) - side.a) * (CalculationP(side) - side.b) * (CalculationP(side) - side.c));
+        }
+    }
+}
