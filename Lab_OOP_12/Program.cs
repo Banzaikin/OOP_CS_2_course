96e4fa0d214b377bdf469bf7a44e88dd17538d70
Bayandin-Kirill <bayandin.kirill123@gmail.com>
2024-02-18 15:44:53 +0500

    Создание меню

--- Lab_OOP_12/Program.cs
+++ Lab_OOP_12/Program.cs
@@ -1,37 +1,82 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Lab_10;
 
 namespace Lab_OOP_12
 {
     class Program
     {
-        static void Main(string[] args)
+        static BinarySearchTree<Organization> CreateTreeRandom()
         {
-            string[] arr = new string[22];
-            arr[0] = "aaa";
-            arr[1] = "aab"; arr[2] = "aba";
-            arr[3] = "baa"; arr[4] = "abb";
-            arr[5] = "bba"; arr[6] = "aac";
-            arr[7] = "caa"; arr[8] = "aca";
-            arr[9] = "abc"; arr[10] = "bca";
-            arr[11] = "acc"; arr[12] = "cca";
-            arr[13] = "bcb"; arr[14] = "bbc";
-            arr[15] = "ccb"; arr[16] = "bbb";
-            arr[17] = "ccc"; arr[18] = "acb";
-            arr[19] = "cba"; arr[20] = "ccb";
-            arr[21] = "acc";
-            HTable ht = new HTable();
-            foreach (string s in arr)
-                ht.Add(s);
-            ht.Print();
-            string findStr;
+            int size = Menu.InputInt("Введите размер массива: ");
+            Organization[] organizations = new Organization[size];
+            for (int i = 0; i < size; i++)
+                organizations[i] = new Organization();
+            BinarySearchTree<Organization> newTree = new BinarySearchTree<Organization>(organizations);
+            return newTree;
+        }
+        static BinarySearchTree<Organization> CreateTreeConsole()
+        {
+            int size = Menu.InputInt("Введите размер массива: ");
+            Organization[] organizations = new Organization[size];
+            for (int i = 0; i < size; i++)
+            {
+                organizations[i] = new Organization();
+                organizations[i].NumEmployess = Menu.InputInt("Введите кол-во сотрудников " + (i + 1) + " организации: ");
+            }
+            BinarySearchTree<Organization> newTree = new BinarySearchTree<Organization>(organizations);
+            return newTree;
+        }
+
+        public static void Main()
+        {
+            var tree = new BinarySearchTree<Organization>(new CustomComparer());
+            string[] menu = {
+                "1. Создание дерева (заполнение рандомными элементами)\n",
+                "2. Создание дерева (заполнение вручную)\n\n",
+                "3. Добавление элемента (Add)\n",
+                "4. Удаление узла (Remove)\n",
+                "5. Поиск элемента по значению (FindNode)\n",
+                "6. Глубокое копирование (Clone)\n",
+                "7. Поверхностное копирование (ShallowCopy)\n",
+                "8. Удаление коллекции из памяти (Clear)\n",
+                "0. Выход из программы\n"
+                };
+            int command;
             do
             {
-                Console.WriteLine("Введите строку для поиска");
-                findStr = Console.ReadLine();
-                if (findStr == "end") continue;
-                if (ht.FindPoint(findStr)) Console.WriteLine("Строка найдена");
-                else Console.WriteLine("Строка не найдена");
-            } while (findStr != "end");
+                //вывод меню
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    Console.WriteLine(menu[i]);
+                }
+                command = Menu.InputInt("Введите команду: ");
+                switch (command)
+                {
+                    case 1: //Создание дерева (заполнение рандомными элементами)
+                        tree = CreateTreeRandom();
+                        break;
+                    case 2: //Создание дерева (заполнение вручную)
+                        tree = CreateTreeConsole();
+                        break;
+                }
+            } while (command != 0) ;
+        }
+    }
+    public class CustomComparer : IComparer<Organization>
+    {
+        public int Compare(Organization? organization1, Organization? organization2)
+        {
+            if (organization1 == null || organization2 == null)
+                return 0;
+
+            if (organization1.NumEmployess > organization2.NumEmployess)
+                return 1;
+            else if (organization1.NumEmployess < organization2.NumEmployess)
+                return -1;
+            else
+                return 0;
         }
     }
 }
